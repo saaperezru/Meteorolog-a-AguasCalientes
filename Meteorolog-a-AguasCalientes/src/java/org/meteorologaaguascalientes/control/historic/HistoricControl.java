aa3a6459cc8225bde96fa693956770260620c209
Juan Hernández <jfhernandeza8@gmail.com>
2012-03-18 16:04:26 -0500

    Merge branch 'historic' into trunk. Package 'org.meteorologaaguascalientes.control' renamed to 'org.meteorologaaguascalientes.control.historic'

--- Meteorolog-a-AguasCalientes/src/java/org/meteorologaaguascalientes/control/historic/HistoricControl.java
+++ Meteorolog-a-AguasCalientes/src/java/org/meteorologaaguascalientes/control/historic/HistoricControl.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.meteorologaaguascalientes.control;
+package org.meteorologaaguascalientes.control.historic;
 
 import java.util.*;
 import org.meteorologaaguascalientes.control.forecast.DefaultForecastImpl;
