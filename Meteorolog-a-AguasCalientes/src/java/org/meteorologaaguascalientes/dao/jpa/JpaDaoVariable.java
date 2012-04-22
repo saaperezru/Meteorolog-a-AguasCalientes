5565275be5f8cd2dbccb1460bba831a902f1bfc7
Santiago Pérez <santiago1991@gmail.com>
2012-04-22 00:10:38 -0500

    Finished lastValue and insertValues functionallity after refactoring.

--- Meteorolog-a-AguasCalientes/src/java/org/meteorologaaguascalientes/dao/jpa/JpaDaoVariable.java
+++ Meteorolog-a-AguasCalientes/src/java/org/meteorologaaguascalientes/dao/jpa/JpaDaoVariable.java
@@ -11,92 +11,91 @@ import org.meteorologaaguascalientes.vo.VariableVo;
 
 public abstract class JpaDaoVariable<E extends VariableVo, F extends VariableMappedSuperclass> implements VariableDao<EntityManager, E> {
 
-    String entityName;
-    String voId;
+	String entityName;
+	String voId;
 
-    public JpaDaoVariable(String entityName, String voId) {
-        this.entityName = entityName;
-        this.voId = voId;
-    }
-    @Override
-    public E insert(DataAccessAdapter<EntityManager> adapter, E vo) throws DataAccessException {
-        checkNullParameters(adapter, "Adapter cannot be null");
-        checkNullParameters(adapter.getDataAccess(), "EntityManager cannot be null");
+	public JpaDaoVariable(String entityName, String voId) {
+		this.entityName = entityName;
+		this.voId = voId;
+	}
 
-        adapter.getDataAccess().persist(voToEntity(vo));
-        return vo;
-    }
+	@Override
+	public E insert(DataAccessAdapter<EntityManager> adapter, E vo) throws DataAccessException {
+		checkNullParameters(adapter, "Adapter cannot be null");
+		checkNullParameters(adapter.getDataAccess(), "EntityManager cannot be null");
 
-    @Override
-    public E update(DataAccessAdapter<EntityManager> adapter, E vo) throws DataAccessException {
-        checkNullParameters(adapter, "Adapter cannot be null");
-        checkNullParameters(adapter.getDataAccess(), "EntityManager cannot be null");
+		adapter.getDataAccess().persist(voToEntity(vo));
+		return vo;
+	}
 
-        adapter.getDataAccess().merge(voToEntity(vo));
-        return vo;
-    }
+	@Override
+	public E update(DataAccessAdapter<EntityManager> adapter, E vo) throws DataAccessException {
+		checkNullParameters(adapter, "Adapter cannot be null");
+		checkNullParameters(adapter.getDataAccess(), "EntityManager cannot be null");
 
-    @Override
-    public void delete(DataAccessAdapter<EntityManager> adapter, Object id) throws DataAccessException {
-        adapter.getDataAccess().remove(findById(adapter, id));
-    }
+		adapter.getDataAccess().merge(voToEntity(vo));
+		return vo;
+	}
 
-    @Override
-    public E findById(DataAccessAdapter<EntityManager> adapter, Object id) throws DataAccessException {
-        checkNullParameters(adapter, "Adapter cannot be null");
-        checkNullParameters(adapter.getDataAccess(), "EntityManager cannot be null");
+	@Override
+	public void delete(DataAccessAdapter<EntityManager> adapter, Object id) throws DataAccessException {
+		adapter.getDataAccess().remove(findById(adapter, id));
+	}
 
-        return (E) adapter.getDataAccess().find(((F) new Object()).getClass(), id).toVo();
-    }
+	@Override
+	public E findById(DataAccessAdapter<EntityManager> adapter, Object id) throws DataAccessException {
+		checkNullParameters(adapter, "Adapter cannot be null");
+		checkNullParameters(adapter.getDataAccess(), "EntityManager cannot be null");
 
-    public abstract F voToEntity(E vo);
+		return (E) adapter.getDataAccess().find(((F) new Object()).getClass(), id).toVo();
+	}
 
-    private void checkNullParameters(Object o, String message) {
-        if (o == null) {
-            throw new NullPointerException(message);
-        }
-    }
+	public abstract F voToEntity(E vo);
 
-    @Override
-    public E getLastValue(DataAccessAdapter<EntityManager> adapter) throws DataAccessException {
-        List<E> list = getLastNValues(adapter, 1);
-        if (list.isEmpty()) {
-            return null;
-        }
-        return list.get(0);
-    }
-    
-    @Override
-    public List<E> getLastNValues(DataAccessAdapter<EntityManager> adapter, int n) throws DataAccessException {
-        if (adapter == null) {
-            throw new IllegalArgumentException("adapter cannot be null");
-        }
-        if (adapter.getDataAccess() == null) {
-            throw new IllegalArgumentException("EntityManager cannot be null");
-        }
-        ArrayList<E> array = new ArrayList<E>();
-        for(Object entity : adapter
-                .getDataAccess().createQuery("SELECT result FROM"+ entityName 
-                +"result ORDER BY result.time DESC").setMaxResults(n).getResultList()){
-            array.add((E)((F)entity).toVo());
-        }
-        return array;
-    }
-    
-    @Override
-    public List<E> getAllValues(DataAccessAdapter<EntityManager> adapter) throws DataAccessException {
-        if (adapter == null) {
-            throw new IllegalArgumentException("adapter cannot be null");
-        }
-        if (adapter.getDataAccess() == null) {
-            throw new IllegalArgumentException("EntityManager cannot be null");
-        }
-        ArrayList<E> array = new ArrayList<E>();
-        for(Object entity : adapter
-                .getDataAccess().createQuery("SELECT result FROM "+ entityName 
-                +" result").getResultList()){
-            array.add((E)((F)entity).toVo());
-        }
-        return array;
-    }
+	private void checkNullParameters(Object o, String message) {
+		if (o == null) {
+			throw new NullPointerException(message);
+		}
+	}
+
+	@Override
+	public E getLastValue(DataAccessAdapter<EntityManager> adapter) throws DataAccessException {
+		List<E> list = getLastNValues(adapter, 1);
+		if (list.isEmpty()) {
+			return null;
+		}
+		return list.get(0);
+	}
+
+	@Override
+	public List<E> getLastNValues(DataAccessAdapter<EntityManager> adapter, int n) throws DataAccessException {
+		if (adapter == null) {
+			throw new IllegalArgumentException("adapter cannot be null");
+		}
+		if (adapter.getDataAccess() == null) {
+			throw new IllegalArgumentException("EntityManager cannot be null");
+		}
+		ArrayList<E> array = new ArrayList<E>();
+		List<F> resultList = adapter.getDataAccess().createQuery("SELECT result FROM " + entityName + " result ORDER BY result.time DESC").setMaxResults(n).getResultList();
+		for (F entity : resultList) {
+			array.add((E) entity.toVo());
+		}
+		return array;
+	}
+
+	@Override
+	public List<E> getAllValues(DataAccessAdapter<EntityManager> adapter) throws DataAccessException {
+		if (adapter == null) {
+			throw new IllegalArgumentException("adapter cannot be null");
+		}
+		if (adapter.getDataAccess() == null) {
+			throw new IllegalArgumentException("EntityManager cannot be null");
+		}
+		ArrayList<E> array = new ArrayList<E>();
+		List<F> resultList = adapter.getDataAccess().createQuery("SELECT result FROM " + entityName + " result").getResultList();
+		for (F entity : resultList) {
+			array.add((E) entity.toVo());
+		}
+		return array;
+	}
 }
--- Meteorolog-a-AguasCalientes/web/insert.jsp
+++ Meteorolog-a-AguasCalientes/web/insert.jsp
@@ -1,3 +1,7 @@
+<%@page import="java.util.logging.Logger"%>
+<%@page import="java.util.logging.Level"%>
+<%@page import="org.meteorologaaguascalientes.businesslogic.facade.HistoricControl"%>
+<%@page import="org.meteorologaaguascalientes.helper.VariablesVoFactory"%>
 <%@page import="org.meteorologaaguascalientes.businesslogic.facade.ServiceFacade"%>
 <%@page import="org.meteorologaaguascalientes.businesslogic.service.ServicesFactory"%>
 <%@page import="com.google.gson.Gson"%>
@@ -8,22 +12,26 @@
 <%! boolean success = false;
 %>
 <%
-    Map<String, String> values = new HashMap<String, String>();
-    String value;
-    for (String key : ServicesFactory.VARIABLES_NAMES) {
-        value = request.getParameter(key);
-        if (value != null) {
-            values.put(key, value);
-        }
-    }
+	JsonObject object = new JsonObject();
+	Gson g = new Gson();
+	try {
+		Map<String, String> values = new HashMap<String, String>();
+		String value;
+		for (String key : VariablesVoFactory.getVariableTypes()) {
+			value = request.getParameter(key);
+			if (value != null) {
+				values.put(key, value);
+			}
+		}
+		ServiceFacade serviceFacade = new ServiceFacade();
+		success = serviceFacade.insertValues(values, request.getParameter("sample"));
+
+		object.addProperty("success", success);
+
+	} catch (Exception e) {
+		Logger.getLogger(HistoricControl.class.getName()).log(Level.SEVERE, null, e);
+	}
 %>
-<%
-    // InsertControl invocation
-    ServiceFacade serviceFacade = new ServiceFacade();
-    success = serviceFacade.insertValues(values, request.getParameter("sample"));
-    
-    Gson g = new Gson();
-    JsonObject object = new JsonObject();
-    object.addProperty("success", success);
+<% // InsertControl invocation
 %>
 <%= g.toJson(object)%>
