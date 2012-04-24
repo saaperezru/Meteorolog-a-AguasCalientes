8baf777dbc0f3bedf5de2c7b305f470a2025e922
Administrator <Administrator@WIN-NNS11C2V3HI>
2012-04-24 16:24:47 -0500

    FIxed null forecast

--- Meteorolog-a-AguasCalientes/src/java/org/meteorologaaguascalientes/businesslogic/facade/ServiceFacade.java
+++ Meteorolog-a-AguasCalientes/src/java/org/meteorologaaguascalientes/businesslogic/facade/ServiceFacade.java
@@ -7,6 +7,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.meteorologaaguascalientes.businesslogic.service.AbstractVariableService;
 import org.meteorologaaguascalientes.businesslogic.service.ServicesFactory;
+import org.meteorologaaguascalientes.control.forecast.ForecastsFactory;
 import org.meteorologaaguascalientes.control.measure.MeasuresFactory;
 import org.meteorologaaguascalientes.da.DataAccessAdapter;
 import org.meteorologaaguascalientes.da.DataAccessException;
@@ -96,7 +97,9 @@ public class ServiceFacade {
         List<SortedMap<Date, Double>> data = new ArrayList<SortedMap<Date, Double>>();
         try {
             DataAccessAdapter da = Config.getInstance().getDataAccessFactory().createDataAccess();
-            data = ServicesFactory.getInstance().getVariableServiceByKey(variable).getDataAndPrediction(da);
+            AbstractVariableService variableServiceByKey = ServicesFactory.getInstance().getVariableServiceByKey(variable);
+            variableServiceByKey.setForecast(ForecastsFactory.getInstance().getForecastById(forecast));
+            data = variableServiceByKey.getDataAndPrediction(da);
         } catch (DataAccessException ex) {
             Logger.getLogger(ServiceFacade.class.getName()).log(Level.SEVERE, null, ex);
         } catch (NoSuchElementException ex) {
